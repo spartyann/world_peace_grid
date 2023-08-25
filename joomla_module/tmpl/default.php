3846901f4fe06770346c3d8a8ba47592d01dac6c
Yann Tassy <tassy.yann@gmail.com>
2023-08-25 17:44:24 +0200

    dev

--- joomla_module/tmpl/default.php
+++ joomla_module/tmpl/default.php
@@ -3,6 +3,9 @@
 // No direct access to this file
 defined('_JEXEC') or die;
 
+$maxW = $params->get('grid_max_width');
+$maxH = $params->get('grid_max_height');
+
 ?>
 
 <style>
@@ -12,58 +15,59 @@ defined('_JEXEC') or die;
 
 	#wp_grid{
 		display: inline-block;
-		max-width:80vw;
-		max-height:75vh;
+		max-width: <?php echo $maxW;?>;
+		max-height: <?php echo $maxH;?>;
 	}
 
 </style>
 
-<div>
-	<form>
-		
-		<div class="d-inline-block m-2">
-			<div class="form-check">
-				<input class="form-check-input" type="radio" name="flexRadioDefault" id="wp_grid_fr" checked>
-				<label class="form-check-label" for="wp_grid_fr">
-				Français
-				</label>
-			</div>
-			<div class="form-check">
-				<input class="form-check-input" type="radio" name="flexRadioDefault" id="wp_grid_en">
-				<label class="form-check-label" for="wp_grid_en">
-				Anglais
-				</label>
-			</div>
+<div class="align-top">
+	<div class="align-top d-inline-block m-2">
+		<div class="form-check">
+			<input class="form-check-input" type="radio" name="flexRadioDefault" id="wp_grid_fr" checked>
+			<label class="form-check-label" for="wp_grid_fr">
+			Français
+			</label>
 		</div>
-		
-		<div class="form-color d-inline-block m-2">
-			<label for="wp_grid_bg_color" class="form-label">Arrière-plan</label>
-			<input type="color" class="form-control form-control-color" id="wp_grid_bg_color" value="#f4f1ec">
+		<div class="form-check">
+			<input class="form-check-input" type="radio" name="flexRadioDefault" id="wp_grid_en">
+			<label class="form-check-label" for="wp_grid_en">
+			Anglais
+			</label>
 		</div>
+	</div>
+	
+	<div class="align-top form-color d-inline-block m-2">
+		<label for="wp_grid_bg_color" class="form-label">Arrière-plan</label>
+		<input type="color" class="form-control form-control-color" id="wp_grid_bg_color" value="#f4f1ec">
+	</div>
 
-		<div class="form-color d-inline-block m-2">
-			<label for="wp_grid_border_color" class="form-label">Bordure</label>
-			<input type="color" class="form-control form-control-color" id="wp_grid_border_color" value="#4b2626">
-		</div>
+	<div class="align-top form-color d-inline-block m-2">
+		<label for="wp_grid_border_color" class="form-label">Bordure</label>
+		<input type="color" class="form-control form-control-color" id="wp_grid_border_color" value="#4b2626">
+	</div>
 
-		<div class="form-color d-inline-block m-2">
-			<label for="wp_grid_icon_color" class="form-label">Icônes</label>
-			<input type="color" class="form-control form-control-color" id="wp_grid_icon_color" value="#4b2626">
-		</div>
+	<div class="align-top form-color d-inline-block m-2">
+		<label for="wp_grid_icon_color" class="form-label">Icônes</label>
+		<input type="color" class="form-control form-control-color" id="wp_grid_icon_color" value="#4b2626">
+	</div>
 
-		<div class="form-color d-inline-block m-2">
-			<label for="wp_grid_text_color" class="form-label">Texte</label>
-			<input type="color" class="form-control form-control-color" id="wp_grid_text_color" value="#4b2626">
-		</div>
+	<div class="align-top form-color d-inline-block m-2">
+		<label for="wp_grid_text_color" class="form-label">Texte</label>
+		<input type="color" class="form-control form-control-color" id="wp_grid_text_color" value="#4b2626">
+	</div>
 
-		
-		<div class="d-inline-block m-2">
-			<p>Taille de la grille dans le PDF (en cm): <input id="wp_grid_size" type="number" class="form-control d-inline" style="width: 70px" min="2" max="50" value="20"/></p>
-			<button class="btn btn-primary" type="button" onclick="exportToPdf(false)">Exporter en PDF A4</button>
-			<button class="btn btn-primary" type="button" onclick="exportToPdf(true)">Exporter en PDF A3</button>
-		</div>
+	
+	<div class="align-top d-inline-block m-2">
+		<label for="wp_grid_text_color" class="form-label">Taille en cm (PDF)</label>
+		<input id="wp_grid_size" type="number" class="form-control " style="width: 70px" min="2" max="50" value="20"/>
+	</div>
 
-	</form>
+	<div class="align-top d-inline-block m-2">
+		<p><button class="btn btn-primary" type="button" onclick="exportToPdf(false)">Exporter en PDF A4</button></p>
+		<p><button class="btn btn-primary" type="button" onclick="exportToPdf(true)">Exporter en PDF A3</button></p>
+	</div>
+
 </div>
 
 
